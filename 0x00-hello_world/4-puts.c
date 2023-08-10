4a3f592815cb8e1f93fe531b74125e9ca8e96b45
root <hmoustapher@gmail.com>
2023-08-10 15:50:09 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  *	main	-it printout statement using put
  *
- * uReturn:	always	(0)
+ * Return:	always	(0)
  *
  */
 int	main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return	(0);
 }
