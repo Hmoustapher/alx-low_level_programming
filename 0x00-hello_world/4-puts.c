49f062528ad78ffa20479a3750ce27c6a936f2ef
root <hmoustapher@gmail.com>
2023-08-10 15:28:38 -0700

    message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include	<stdio.h>
+/**
+ * desccription of main method
+ *
+ * retun	always	(0)
+ *
+ * */
 int	main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return	(0);
 }
