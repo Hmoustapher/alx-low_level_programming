ed46aebff18fb12868fd4dfbccf1aa9b58197aae
root <hmoustapher@gmail.com>
2023-08-10 12:59:59 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,13 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the message 
- *              using the puts function and ends with a return statement.
- * Return: always (0)
- 
-int main(void){
-puts("\"Programming is like building a multilingual puzzle,\");
-    return (0);
+ * Description: Prints the message "with proper grammar, but the outcome is a piece of art,",
+ *              using the printf function and ends with a return statement.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
