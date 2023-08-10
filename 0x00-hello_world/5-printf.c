63b04aaaf6fc9c1c4eb9366db040665108a0ecb4
root <hmoustapher@gmail.com>
2023-08-10 16:36:46 -0700

    d

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 /**
  * main	-	printing output with printf
  *
- * Description:	print message	"leave aspace"
+ * Description:	print message	"leave	aspace"
  *
  * Return:	always	(0)	Success
  */
 int	main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with	proper	grammar,	but	the	outcome	is	a	piece	of	art,\n");
 	return (0);
 }
