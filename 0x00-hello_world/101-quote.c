14f0e780c0eb34c4cbb38d27c73885d9ae9d0df6
root <hmoustapher@gmail.com>
2023-08-10 18:12:07 -0700

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <unistd.h>
-
+#include	<stdio.h>
+#include	<unistd/h>
 /**
- * main - Entry point of the program
+ * main	-	starting	point
  *
- * Description: Prints the message "and that piece of art is useful\" - Dora Korpar, 2015-10-19,"
- *              to the standard error and returns 1.
- * Return: Always 1 (Error)
+ * Return:	always	(1)
+ *
  */
-int main(void)
+int	main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
-
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
+	return (1);
 }
