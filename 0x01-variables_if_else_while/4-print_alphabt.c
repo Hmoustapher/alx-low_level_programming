a4af195196321f458106207ef733de2d307e8caf
root <hmoustapher@gmail.com>
2023-08-12 09:12:42 -0700

    S

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -m print alpj
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			 putchar(letter);
+		}
+	letter++;
+	}
+	putchar('\n');
+	return (0);
+}
