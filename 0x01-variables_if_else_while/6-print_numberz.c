4eb327561dbf747c31ede1c1f2bc18c047a66c85
root <hmoustapher@gmail.com>
2023-08-12 09:41:43 -0700

    d

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -m what is betty 
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
