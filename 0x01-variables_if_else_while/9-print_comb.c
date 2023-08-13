d6043fe9258c1fcd714a86420f08b7f5619060eb
root <hmoustapher@gmail.com>
2023-08-13 00:13:27 -0700

    d

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main mdescription
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d <= '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
