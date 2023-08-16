25c5bb9d894a17f8aee4726f7c8ad45a9296fcd7
root <hmoustapher@gmail.com>
2023-08-16 09:39:15 -0700

    d

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* month: month in number format
+* day: day of month
+* Return: day of year
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
