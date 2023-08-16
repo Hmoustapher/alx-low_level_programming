77b1a104b6f11d20a9d3915f5ddc8cbde903524f
root <hmoustapher@gmail.com>
2023-08-16 09:56:55 -0700

    d

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* month: month in number format
-* day: day of month
-* Return: day of year
+* main - Test function for positive or negative
+* Return: 0
 */
 int main(void)
 {
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
