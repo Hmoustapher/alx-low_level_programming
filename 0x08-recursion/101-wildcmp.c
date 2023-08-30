3d237f1d222cdae5e3b7bdba3f3629dc4ede4662
root <hmoustapher@gmail.com>
2023-08-30 02:49:53 -0700

    s

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
+ */
+int wildcmp(char *sa1, char *sa2)
+{
+	if (*sa1 == '\0')
+	{
+		if (*sa2 != '\0' && *sa2 == '*')
+		{
+			return (wildcmp(sa1, sa2 + 1));
+		}
+		return (*sa2 == '\0');
+	}
+
+	if (*sa2 == '*')
+	{
+		return (wildcmp(sa1 + 1, sa2) || wildcmp(sa1, sa2 + 1));
+	}
+	else if (*sa1 == *sa2)
+	{
+		return (wildcmp(sa1 + 1, sa2 + 1));
+	}
+	return (0);
+}
+
